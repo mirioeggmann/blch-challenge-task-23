9b094e7c20ba6492a0e051a16c1abf9a9ecdd469
Dario Glasl <dglasl@swisslex.ch>
2023-11-14 20:52:01 +0100

    Update Marketplace.tsx

--- src/components/templates/marketplace/Marketplace.tsx
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -1,16 +1,20 @@
 import {Typography} from "@web3uikit/core";
-import {Heading} from "@chakra-ui/react";
+import {Box, Button, Heading, Input, useColorModeValue} from "@chakra-ui/react";
 
 const Marketplace = () => {
+    const hoverTrColor = useColorModeValue('gray.100', 'gray.700');
 
-  return (
-      <>
-        <Heading size="lg" marginBottom={6}>
-          Marketplace
-        </Heading>
-        <Typography>This is the Marketplace</Typography>
-      </>
-  );
+    return (
+        <>
+            <Heading size="lg" marginBottom={6}>
+                Marketplace
+            </Heading>
+            <Box border="2px" borderColor={hoverTrColor} borderRadius="xl" padding="24px 18px">
+                <Input placeholder='Address'/>
+                <Button>Buy</Button>
+            </Box>
+        </>
+    );
 };
 
 export default Marketplace;
