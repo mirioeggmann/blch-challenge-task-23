1256e5f9b89b049d9c3cd85e49ed89ea663aeef2
Dario Glasl <dglasl@swisslex.ch>
2023-11-14 20:48:14 +0100

    Added marketplace to navigation and new page

---
+++ pages/marketplace.tsx
@@ -0,0 +1,13 @@
+import { Default } from 'components/layouts/Default';
+import {Typography} from "@web3uikit/core";
+import Marketplace from "../src/components/templates/marketplace/Marketplace";
+
+const MarketplacePage = () => {
+    return (
+        <Default pageName="Marketplace">
+            <Marketplace></Marketplace>
+        </Default>
+    );
+};
+
+export default MarketplacePage;
---
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -0,0 +1,16 @@
+import {Typography} from "@web3uikit/core";
+import {Heading} from "@chakra-ui/react";
+
+const Marketplace = () => {
+
+  return (
+      <>
+        <Heading size="lg" marginBottom={6}>
+          Marketplace
+        </Heading>
+        <Typography>This is the Marketplace</Typography>
+      </>
+  );
+};
+
+export default Marketplace;
