9816e3b56d98bee2b4f27e6c96657d39378d6d3c
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2023-11-20 21:43:02 +0100

    update failing deps

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['@moralisweb3', 'plugin:@next/next/recommended', 'plugin:cypress/recommended'],
-  ignorePatterns: ['**/build/**/*'],
-  rules: {
-    'no-console': 'off',
-  },
-};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,31 @@
-import {ChakraProvider} from '@chakra-ui/react';
-import {createClient, WagmiConfig} from 'wagmi';
-import {configureChains} from '@wagmi/core';
+import { ChakraProvider } from '@chakra-ui/react';
+import { createConfig, WagmiConfig } from 'wagmi';
+import { configureChains } from '@wagmi/core';
 import {
+  arbitrum,
+  arbitrumGoerli,
+  avalanche,
+  avalancheFuji,
+  bsc,
+  bscTestnet,
+  fantom,
+  fantomTestnet,
+  foundry,
+  goerli,
+  mainnet,
+  optimism,
+  optimismGoerli,
+  polygon,
+  polygonMumbai,
+  sepolia,
+} from '@wagmi/core/chains';
+import { extendTheme } from '@chakra-ui/react';
+import { publicProvider } from 'wagmi/providers/public';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app';
+
+const { publicClient, webSocketPublicClient } = configureChains(
+  [
     arbitrum,
     arbitrumGoerli,
     avalanche,
@@ -18,57 +42,33 @@ import {
     polygon,
     polygonMumbai,
     sepolia,
-} from '@wagmi/core/chains';
-import {extendTheme} from '@chakra-ui/react';
-import {publicProvider} from 'wagmi/providers/public';
-import {SessionProvider} from 'next-auth/react';
-import type {AppProps} from 'next/app';
-
-const {provider, webSocketProvider} = configureChains(
-    [
-        arbitrum,
-        arbitrumGoerli,
-        avalanche,
-        avalancheFuji,
-        bsc,
-        bscTestnet,
-        fantom,
-        fantomTestnet,
-        foundry,
-        goerli,
-        mainnet,
-        optimism,
-        optimismGoerli,
-        polygon,
-        polygonMumbai,
-        sepolia,
-    ],
-    [publicProvider()],
+  ],
+  [publicProvider()],
 );
 
-const client = createClient({
-    provider,
-    webSocketProvider,
-    autoConnect: true,
+const client = createConfig({
+  publicClient,
+  webSocketPublicClient,
+  autoConnect: true,
 });
 
 const config = {
-    initialColorMode: 'dark',
-    useSystemColorMode: false,
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
 };
 
-const theme = extendTheme({config});
+const theme = extendTheme({ config });
 
-const MyApp = ({Component, pageProps}: AppProps) => {
-    return (
-        <ChakraProvider resetCSS theme={theme}>
-            <WagmiConfig client={client}>
-                <SessionProvider session={pageProps.session} refetchInterval={0}>
-                    <Component {...pageProps} />
-                </SessionProvider>
-            </WagmiConfig>
-        </ChakraProvider>
-    );
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <WagmiConfig config={client}>
+        <SessionProvider session={pageProps.session} refetchInterval={0}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </WagmiConfig>
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
