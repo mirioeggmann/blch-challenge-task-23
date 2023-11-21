bb3b5c4b237eedd7bb6e3f9d571e381e73e86230
Dario Glasl <dglasl@swisslex.ch>
2023-11-21 21:06:03 +0100

    exchange address and abi added

--- src/components/templates/marketplace/Marketplace.tsx
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -1,7 +1,16 @@
 import {Box, Button, Heading, HStack, Input, useColorModeValue} from "@chakra-ui/react";
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import {FormEvent} from "react";
 
 const Marketplace = () => {
     const hoverTrColor = useColorModeValue('gray.100', 'gray.700');
+
+    async function buyNft(event: any) {
+        event.preventDefault();
+        console.log(event.target[0].value);
+        console.log(process.env.EXCHANGE_ADDRESS);
+    }
 
     return (
         <>
@@ -9,10 +18,19 @@ const Marketplace = () => {
                 Marketplace
             </Heading>
             <Box border="2px" borderColor={hoverTrColor} borderRadius="xl" padding="24px 18px">
-                <HStack>
-                    <Input placeholder='Address'/>
-                    <Button>Buy</Button>
-                </HStack>
+                <form onSubmit={buyNft}>
+                    <HStack>
+                        <label htmlFor={"address"}>Address</label>
+                        <Input
+                            id={"address"}
+                            type={"text"}
+                            placeholder='Address'
+                            name={"address"}
+                            required
+                        />
+                        <Button type={"submit"}>Buy</Button>
+                    </HStack>
+                </form>
             </Box>
         </>
     );
