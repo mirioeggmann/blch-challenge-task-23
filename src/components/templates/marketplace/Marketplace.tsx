9b97241ff92d7048d53fe06170e7fee956e76824
Dario Glasl <dglasl@swisslex.ch>
2023-11-26 19:22:33 +0100

    load listings from contract

--- src/components/templates/marketplace/Marketplace.tsx
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -2,6 +2,10 @@ import {Box, Grid, Heading} from "@chakra-ui/react";
 import {useNetwork} from "wagmi";
 import {useEvmWalletNFTs} from "@moralisweb3/next";
 import {NFTCard} from "../../modules";
+import {useContractRead} from "wagmi";
+import {loadAbi} from "../../../utils/ethereumUtils";
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import Moralis from "moralis";
 
 const Marketplace = () => {
     const exchangeContractAddress = '0x31026ebe2841825cb0639aa2e138f770d3b1a4f9';
@@ -11,6 +15,31 @@ const Marketplace = () => {
         address: exchangeContractAddress,
         chain: chain?.id,
     });
+
+    console.log(nfts)
+
+    const { data, isError, isLoading } = useContractRead({
+        address: exchangeContractAddress,
+        abi: loadAbi(),
+        functionName: 'getAllListings',
+    })
+
+    console.log(data)
+
+    // if (!isLoading || !data) {
+    //     for (const d of data!) {
+    //         getSingleNft(d.nftContract, d.tokenId);
+    //     }
+    // }
+    //
+    // async function getSingleNft (address: string, tokenId: string){
+    //     const response = await Moralis.EvmApi.nft.getNFTMetadata({
+    //         address: address,
+    //         chain: EvmChain.SEPOLIA,
+    //         tokenId: tokenId
+    //     });
+    //     console.log(response);
+    // }
 
     return (
         <>
