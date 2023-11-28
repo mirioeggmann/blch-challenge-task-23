9b9809bcf230f157855a1edcbda24ea649ab887f
Dario Glasl <dglasl@swisslex.ch>
2023-11-28 22:40:35 +0100

    Extracted nft transfer table

--- src/components/templates/transfers/NFT/NFTTransfers.tsx
+++ src/components/templates/transfers/NFT/NFTTransfers.tsx
@@ -16,68 +16,18 @@ import {useSession} from 'next-auth/react';
 import {useEffect} from 'react';
 import {getEllipsisTxt} from 'utils/format';
 import {useNetwork} from 'wagmi';
+import NftTransferTable from "./NftTransferTable";
 
 const NFTTransfers = () => {
-    const hoverTrColor = useColorModeValue('gray.100', 'gray.700');
     const {data} = useSession();
     const {chain} = useNetwork();
-    const {data: transfers} = useEvmWalletNFTTransfers({
-        address: data?.user?.address,
-        chain: chain?.id,
-    });
-
-    useEffect(() => console.log('transfers: ', transfers), [transfers]);
 
     return (
         <>
             <Heading size="lg" marginBottom={6}>
-                NFT Transfers
+                Your NFT Transfers
             </Heading>
-            {transfers?.length ? (
-                <Box border="2px" borderColor={hoverTrColor} borderRadius="xl" padding="24px 18px">
-                    <TableContainer w={'full'}>
-                        <Table>
-                            <Thead>
-                                <Tr>
-                                    <Th>Token</Th>
-                                    <Th>Token Id</Th>
-                                    <Th>From</Th>
-                                    <Th>To</Th>
-                                    <Th>Type</Th>
-                                    <Th>Date</Th>
-                                    <Th isNumeric>Tx Hash</Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                {transfers?.map((transfer, key) => (
-                                    <Tr key={key} _hover={{bgColor: hoverTrColor}} cursor="pointer">
-                                        <Td>{getEllipsisTxt(transfer?.tokenAddress.checksum)}</Td>
-                                        <Td>{transfer?.tokenId}</Td>
-                                        <Td>{getEllipsisTxt(transfer?.fromAddress?.checksum)}</Td>
-                                        <Td>{getEllipsisTxt(transfer?.toAddress.checksum)}</Td>
-                                        <Td>{transfer.contractType}</Td>
-                                        <Td>{new Date(transfer.blockTimestamp).toLocaleDateString()}</Td>
-                                        <Td isNumeric>{getEllipsisTxt(transfer.transactionHash, 2)}</Td>
-                                    </Tr>
-                                ))}
-                            </Tbody>
-                            <Tfoot>
-                                <Tr>
-                                    <Th>Token</Th>
-                                    <Th>Token Id</Th>
-                                    <Th>From</Th>
-                                    <Th>To</Th>
-                                    <Th>Type</Th>
-                                    <Th>Date</Th>
-                                    <Th isNumeric>Tx Hash</Th>
-                                </Tr>
-                            </Tfoot>
-                        </Table>
-                    </TableContainer>
-                </Box>
-            ) : (
-                <Box>Looks Like you do not have any NFT transfers</Box>
-            )}
+            <NftTransferTable address={data?.user?.address} chain={chain?.id}></NftTransferTable>
         </>
     );
 };
