867e9e2d19d50a831af7e1d83845a06991b48393
Dario Glasl <dglasl@swisslex.ch>
2023-11-28 21:17:41 +0100

    read listings and display on marketplace

--- src/components/templates/marketplace/Marketplace.tsx
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -1,62 +1,36 @@
-import { Box, Grid, Heading } from '@chakra-ui/react';
-import { useNetwork } from 'wagmi';
-import { useEvmWalletNFTs } from '@moralisweb3/next';
-import { NFTCard } from '../../modules';
-import { useContractRead } from 'wagmi';
-import { loadAbi } from '../../../utils/ethereumUtils';
-import { EvmChain } from '@moralisweb3/common-evm-utils';
-import Moralis from 'moralis';
+import {Box, Grid, Heading} from '@chakra-ui/react';
+import {useEvmNFTMetadata} from '@moralisweb3/next';
+import {useContractRead} from 'wagmi';
+import {loadAbi} from '../../../utils/ethereumUtils';
+import MarketplaceElement from "./MarketplaceElement";
 
 const Marketplace = () => {
-  const exchangeContractAddress = '0x654406b8a47Ae366e35D100695a95D9aa26b3eC4';
+    const exchangeContractAddress = '0x654406b8a47Ae366e35D100695a95D9aa26b3eC4';
 
-  const { chain } = useNetwork();
-  const { data: nfts } = useEvmWalletNFTs({
-    address: exchangeContractAddress,
-    chain: chain?.id,
-  });
+    const { data: listings } = useContractRead({
+        address: exchangeContractAddress,
+        abi: loadAbi(),
+        functionName: 'getAllListings',
+    });
 
-  console.log(nfts);
+    console.log(listings);
 
-  const { data, isError, isLoading } = useContractRead({
-    address: exchangeContractAddress,
-    abi: loadAbi(),
-    functionName: 'getAllListings',
-  });
-
-  console.log(data);
-
-  // if (!isLoading || !data) {
-  //     for (const d of data!) {
-  //         getSingleNft(d.nftContract, d.tokenId);
-  //     }
-  // }
-  //
-  // async function getSingleNft (address: string, tokenId: string){
-  //     const response = await Moralis.EvmApi.nft.getNFTMetadata({
-  //         address: address,
-  //         chain: EvmChain.SEPOLIA,
-  //         tokenId: tokenId
-  //     });
-  //     console.log(response);
-  // }
-
-  return (
-    <>
-      <Heading size="lg" marginBottom={6}>
-        Marketplace
-      </Heading>
-      {nfts?.length ? (
-        <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
-          {nfts.map((nft, key) => (
-            <NFTCard nft={nft} key={key} isSelling={true} />
-          ))}
-        </Grid>
-      ) : (
-        <Box>Looks the Exchange does not have any NFTs</Box>
-      )}
-    </>
-  );
+    return (
+        <>
+            <Heading size="lg" marginBottom={6}>
+                Marketplace
+            </Heading>
+            {listings?.length ? (
+                <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
+                    {listings.slice(1).map((listing, key) => (
+                        <MarketplaceElement key={key} tokenId={listing.tokenId} nftContractAdress={listing.nftContract} price={listing.price} />
+                    ))}
+                </Grid>
+            ) : (
+                <Box>Looks the Exchange does not have any NFTs</Box>
+            )}
+        </>
+    );
 };
 
 export default Marketplace;
