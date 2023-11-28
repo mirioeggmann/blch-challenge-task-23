b57ded038459d2df47b3c7a878d8ac98d161bc3f
Dario Glasl <dglasl@swisslex.ch>
2023-11-28 22:55:14 +0100

    added event creation page

---
+++ src/components/templates/eventCreation/EventCreation.tsx
@@ -0,0 +1,15 @@
+import {Heading} from "@chakra-ui/react";
+
+
+const EventCreation = () => {
+
+    return (
+        <>
+            <Heading size="lg" marginBottom={6}>
+                Create Event
+            </Heading>
+        </>
+    );
+};
+
+export default EventCreation;
