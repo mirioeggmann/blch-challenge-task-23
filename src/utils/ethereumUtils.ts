ad22502fbc791e3c0e593c49c7fea0aaf3441f2a
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2023-11-29 23:27:03 +0100

    cleanup wave

--- src/utils/ethereumUtils.ts
+++ src/utils/ethereumUtils.ts
@@ -1,8 +1,3 @@
-import {getAbiItem} from "viem";
-import json5 from "json5";
-import {useContractRead, useContractWrite} from "wagmi";
-
 export function loadAbi() {
-    const json = require('../../abi/exchange_abi.json');
-    return json;
+    return require('../../abi/exchange_abi.json');
 }
