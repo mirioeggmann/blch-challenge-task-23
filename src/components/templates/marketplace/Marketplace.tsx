1554d9523f0aaceb2aae8056177c6f528c7d1d1a
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2023-11-29 02:16:46 +0100

    working exchange without skipping the first and also the last is visible

--- src/components/templates/marketplace/Marketplace.tsx
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -5,7 +5,7 @@ import MarketplaceElement from './MarketplaceElement';
 import NftTransferTable from '../transfers/NFT/NftTransferTable';
 
 const Marketplace = () => {
-    const exchangeContractAddress = '0x7f13f94c59893ea456a39b5299f74aa0b307695e';
+    const exchangeContractAddress = '0x726A8bBFeE820aa8cbDa08dbe6ba5d06A3A3eAA9';
     const { chain } = useNetwork();
 
     const { data: listings } = useContractRead({
@@ -26,7 +26,7 @@ const Marketplace = () => {
             </Heading>
             {listings?.length ? (
                 <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
-                    {listings.slice(1).map((listing, key) => {
+                    {listings.map((listing, key) => {
                         if (!listing.isSold) {
                             return (
                                 <MarketplaceElement
