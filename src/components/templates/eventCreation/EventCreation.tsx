82d7b85d7b3defcfc0180170364bfe460f0e1edc
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2023-11-30 16:36:30 +0100

    add first part of listing, still need the bulkListing function but we first need to add it to contract

--- src/components/templates/eventCreation/EventCreation.tsx
+++ src/components/templates/eventCreation/EventCreation.tsx
@@ -2,18 +2,25 @@ import { Box, Button, Heading, HStack, Input } from '@chakra-ui/react';
 import { deployContract } from 'viem/contract';
 import { createWalletClient, custom, http } from 'viem';
 import { sepolia } from '@wagmi/core/chains';
-import { loadAbiNftContract, loadBytecodeNftContract } from '../../../utils/ethereumUtils';
+import { loadAbi, loadAbiNftContract, loadBytecodeNftContract } from '../../../utils/ethereumUtils';
+import { useContractWrite } from 'wagmi';
 
 const EventCreation = () => {
+    // TODO muss noch auf range gewechselt werden und nicht single createListing
+    const exchangeContractAddress = '0x726A8bBFeE820aa8cbDa08dbe6ba5d06A3A3eAA9';
+    const { write: createBulkListingWrite } = useContractWrite({
+        address: exchangeContractAddress,
+        abi: loadAbi(),
+        functionName: 'createListing',
+    });
+
     async function onSubmit(event: any) {
         event.preventDefault();
 
         const name = event.target[0].value;
         const abbr = event.target[1].value as string;
+        const amount = event.target[3].value;
         const price = event.target[2].value;
-        const amount = event.target[3].value; // wird in step 2 benötigt
-
-        // TODO call 1: creation of contract, cleanup to insert form stuff etc.
 
         const client = createWalletClient({
             chain: sepolia,
@@ -32,7 +39,11 @@ const EventCreation = () => {
 
         console.log(result); // contract wird erfolgreich erstellt, achtung return wert ist nicht contract addresse sondern tx adresse, welche aufzeigt von wem der contract erstellt wurde
 
-        // TODO call 2: vermutlich in neuem form für list event contractwrite (analog zu create listing - aber mit bulk listing funktion) -> auf exchange contract
+        // TODO hier noch dynamisch die neue contract adresse hinterlegen, die vorhin neu erzeut wurde
+        // TODO auslagern in eigene funktion + (form?)
+        createBulkListingWrite({
+            args: ['0x4464Da745f26CD714fA9E4094b9CB8ED9F453612', amount, price],
+        });
     }
 
     return (
@@ -54,8 +65,6 @@ const EventCreation = () => {
                     </HStack>
                     <HStack marginTop={2}>
                         <Button type={'submit'}>Create</Button>
-                        {/*TODO auslagern in sinnvolle view*/}
-                        <Button type={'submit'}>List Event</Button>
                     </HStack>
                 </form>
             </Box>
