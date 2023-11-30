8ec56eb1e6e9f4bf3699ee64148c3089f0e96387
Dario Glasl <dglasl@swisslex.ch>
2023-11-30 22:07:19 +0100

    Home removed, login required before marketplace opens, unused buttons removed

--- src/components/templates/home/Home.tsx
+++
@@ -1,9 +0,0 @@
-import {Typography} from "@web3uikit/core";
-
-const Home = () => {
-    return (
-        <Typography>This is the startpage</Typography>
-    );
-};
-
-export default Home;
--- src/components/templates/marketplace/Marketplace.tsx
+++ src/components/templates/marketplace/Marketplace.tsx
@@ -3,10 +3,13 @@ import { useContractRead, useNetwork } from 'wagmi';
 import { loadAbi } from '../../../utils/ethereumUtils';
 import MarketplaceElement from './MarketplaceElement';
 import NftTransferTable from '../transfers/NFT/NftTransferTable';
+import {useSession} from "next-auth/react";
+import {Typography} from "@web3uikit/core";
 
 const Marketplace = () => {
     const exchangeContractAddress = '0x3288b0e0194b2b74571a62a344c67b7d62637f7b';
     const { chain } = useNetwork();
+    const session = useSession();
 
     const { data: listings } = useContractRead({
         address: exchangeContractAddress,
@@ -16,33 +19,39 @@ const Marketplace = () => {
 
     return (
         <>
-            <Heading size="lg" marginBottom={6}>
-                Marketplace History
-            </Heading>
-            <NftTransferTable address={exchangeContractAddress} chain={chain?.id}></NftTransferTable>
+            {
+                session.status === "unauthenticated"
+                    ? <Typography>Log In to join the Marketplace!</Typography>
+                    : <>
+                        <Heading size="lg" marginBottom={6}>
+                            Marketplace History
+                        </Heading>
+                        <NftTransferTable address={exchangeContractAddress} chain={chain?.id}></NftTransferTable>
 
-            <Heading size="lg" marginTop={6} marginBottom={6}>
-                Marketplace
-            </Heading>
-            {listings?.length ? (
-                <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
-                    {listings.map((listing, key) => {
-                        if (!listing.isSold) {
-                            return (
-                                <MarketplaceElement
-                                    key={key}
-                                    tokenId={listing.tokenId}
-                                    id={listing.id}
-                                    nftContractAdress={listing.nftContract}
-                                    price={listing.price}
-                                />
-                            );
-                        }
-                    })}
-                </Grid>
-            ) : (
-                <Box>Looks like the Exchange does not have any NFTs</Box>
-            )}
+                        <Heading size="lg" marginTop={6} marginBottom={6}>
+                            Marketplace
+                        </Heading>
+                        {listings?.length ? (
+                            <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
+                                {listings.map((listing, key) => {
+                                    if (!listing.isSold) {
+                                        return (
+                                            <MarketplaceElement
+                                                key={key}
+                                                tokenId={listing.tokenId}
+                                                id={listing.id}
+                                                nftContractAdress={listing.nftContract}
+                                                price={listing.price}
+                                            />
+                                        );
+                                    }
+                                })}
+                            </Grid>
+                        ) : (
+                            <Box>Looks like the Exchange does not have any NFTs</Box>
+                        )}
+                    </>
+            }
         </>
     );
 };
