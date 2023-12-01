00bb0a53d5ebc442d3f701e4a0057ddc49cd89e0
Dario Glasl <dglasl@swisslex.ch>
2023-12-01 07:47:54 +0100

    login required for everything

--- src/components/templates/balances/NFT/NFTBalances.tsx
+++ src/components/templates/balances/NFT/NFTBalances.tsx
@@ -4,10 +4,13 @@ import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import { useNetwork } from 'wagmi';
 import NFTCardSell from '../../../modules/NFTCard/NFTCardSell';
+import {Typography} from "@web3uikit/core";
 
 const NFTBalances = () => {
     const { data } = useSession();
     const { chain } = useNetwork();
+    const session = useSession();
+
     const { data: nfts } = useEvmWalletNFTs({
         address: data?.user?.address,
         chain: chain?.id,
@@ -20,15 +23,19 @@ const NFTBalances = () => {
             <Heading size="lg" marginBottom={6}>
                 Tickets
             </Heading>
-            {nfts?.length ? (
-                <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
-                    {nfts.map((nft, key) => (
-                        <NFTCardSell nft={nft} key={key} />
-                    ))}
-                </Grid>
-            ) : (
-                <Box>Looks Like you do not have any NFTs</Box>
-            )}
+            {
+                session.status === "unauthenticated"
+                    ? <Typography>Log In to see your Tickets!</Typography>
+                    : nfts?.length
+                        ? (
+                            <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
+                                {nfts.map((nft, key) => (
+                                    <NFTCardSell nft={nft} key={key} />
+                                ))}
+                            </Grid>
+                        )
+                        : <Box>Looks Like you do not have any NFTs</Box>
+            }
         </>
     );
 };
