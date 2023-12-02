4d6da3fb81a8218168c8762f2fdc9a107cfbebd9
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2023-12-02 08:28:17 +0100

    fix deployment

--- src/components/templates/balances/NFT/NFTBalances.tsx
+++ src/components/templates/balances/NFT/NFTBalances.tsx
@@ -1,9 +1,9 @@
 import { Box, Grid, Heading } from '@chakra-ui/react';
 import { useEvmWalletNFTs } from '@moralisweb3/next';
 import { useSession } from 'next-auth/react';
-import {useNetwork, useWaitForTransaction} from 'wagmi';
+import { useNetwork } from 'wagmi';
 import NFTCardSell from '../../../modules/NFTCard/NFTCardSell';
-import {Typography} from "@web3uikit/core";
+import { Typography } from '@web3uikit/core';
 
 const NFTBalances = () => {
     const { data } = useSession();
@@ -25,20 +25,21 @@ const NFTBalances = () => {
             <Heading size="lg" marginBottom={6}>
                 Tickets
             </Heading>
-            <Typography>If you want to resell your ticket, you can put tickets on to the marketplace by defining a selling price and than click on the button "offer"</Typography>
-            {
-                session.status === "unauthenticated"
-                    ? <Typography>Log In to see your Tickets!</Typography>
-                    : nfts?.length
-                        ? (
-                            <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
-                                {nfts.map((nft, key) => (
-                                    <NFTCardSell nft={nft} key={key} />
-                                ))}
-                            </Grid>
-                        )
-                        : <Box>Looks Like you do not have any NFTs</Box>
-            }
+            <Typography>
+                If you want to resell your ticket, you can put tickets on to the marketplace by defining a selling price
+                and than click on the button &quot;offer&quot;
+            </Typography>
+            {session.status === 'unauthenticated' ? (
+                <Typography>Log In to see your Tickets!</Typography>
+            ) : nfts?.length ? (
+                <Grid templateColumns="repeat(auto-fit, minmax(280px, 1fr))" gap={6}>
+                    {nfts.map((nft, key) => (
+                        <NFTCardSell nft={nft} key={key} />
+                    ))}
+                </Grid>
+            ) : (
+                <Box>Looks Like you do not have any NFTs</Box>
+            )}
         </>
     );
 };
