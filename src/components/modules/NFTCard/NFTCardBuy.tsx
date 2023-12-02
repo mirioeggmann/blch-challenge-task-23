5846bb889b966c86de1225b598d6f286779f377d
Dario Glasl <dglasl@swisslex.ch>
2023-12-02 11:41:13 +0100

    added UX improvements for buy and sell

--- src/components/modules/NFTCard/NFTCardBuy.tsx
+++ src/components/modules/NFTCard/NFTCardBuy.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, HStack, SimpleGrid, useColorModeValue } from '@chakra-ui/react';
+import {Box, Button, HStack, SimpleGrid, Spinner, useColorModeValue} from '@chakra-ui/react';
 import { EvmNft } from '@moralisweb3/common-evm-utils';
 import { Eth } from '@web3uikit/icons';
-import { FC } from 'react';
-import { useContractWrite } from 'wagmi';
+import {FC, useState} from 'react';
+import {useContractWrite, useWaitForTransaction} from 'wagmi';
 import { loadAbi } from '../../../utils/ethereumUtils';
+import {Typography} from "@web3uikit/core";
 
 export interface NFTCardParams {
     key: number;
@@ -19,22 +20,26 @@ const NFTCardBuy: FC<NFTCardParams> = ({ nft, buyerPrice, id }) => {
 
     const exchangeContractAddress = process.env.NEXT_PUBLIC_EXCHANGE_ADDRESS as `0x{string}`;
 
-    const { write: buyNFTWrite } = useContractWrite({
+    let [txHash, setTxHash] = useState<string | null>(null);
+
+    const { writeAsync: buyNFTWrite } = useContractWrite({
         address: exchangeContractAddress,
         abi: loadAbi(),
         functionName: 'buyNFT',
     });
 
-    function buyNFT(listingId: number) {
-        buyNFTWrite({
-            args: [listingId],
+    const { data: txReceipt, isLoading: txIsLoading } = useWaitForTransaction({
+        hash: txHash as `0x{string}`,
+        confirmations: 2,
+    });
+
+    async function buyNFT() {
+        const result = await buyNFTWrite({
+            args: [id],
             value: buyerPrice as bigint,
         });
-    }
 
-    function buy(nft: EvmNft): void {
-        // TODO change this to listingId (from exchange contract not tokenId from nft contract)
-        buyNFT(id as number);
+        setTxHash(result.hash);
     }
 
     return (
@@ -46,53 +51,61 @@ const NFTCardBuy: FC<NFTCardParams> = ({ nft, buyerPrice, id }) => {
             borderWidth="1px"
             borderColor={borderColor}
         >
-            <Box mt="1" fontWeight="semibold" as="h4" noOfLines={1} marginTop={2}>
-                {nft.name}
-            </Box>
-            <HStack alignItems={'center'}>
-                <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="smaller">
-                    {nft.contractType}
-                </Box>
+            {
+                (txReceipt === null || txReceipt === undefined)
+                    ? txIsLoading
+                        ? <><Typography>Buying in process...</Typography><Spinner /></>
+                        : <>
+                            <Box mt="1" fontWeight="semibold" as="h4" noOfLines={1} marginTop={2}>
+                                {nft.name}
+                            </Box>
+                            <HStack alignItems={'center'}>
+                                <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="smaller">
+                                    {nft.contractType}
+                                </Box>
 
-                <Eth fontSize="20px" />
-            </HStack>
-            <SimpleGrid columns={2} spacing={4} bgColor={descBgColor} padding={2.5} borderRadius="xl" marginTop={2}>
-                <Box>
-                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
-                        Token ID
-                    </Box>
-                    <Box as="h4" noOfLines={1} fontSize="sm">
-                        {nft.tokenId}
-                    </Box>
-                </Box>
-                <Box>
-                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
-                        Amount
-                    </Box>
-                    <Box as="h4" noOfLines={1} fontSize="sm">
-                        {nft.amount}
-                    </Box>
-                </Box>
-                <Box>
-                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
-                        Price
-                    </Box>
-                    <Box as="h4" noOfLines={1} fontSize="sm">
-                        {buyerPrice.toString()} Wei
-                    </Box>
-                </Box>
-                <Box>
-                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
-                        ExchangeListingId
-                    </Box>
-                    <Box as="h4" noOfLines={1} fontSize="sm">
-                        {id.toString()}
-                    </Box>
-                </Box>
-            </SimpleGrid>
-            <HStack marginTop={2}>
-                <Button onClick={() => buy(nft)}>Buy</Button>
-            </HStack>
+                                <Eth fontSize="20px" />
+                            </HStack>
+                            <SimpleGrid columns={2} spacing={4} bgColor={descBgColor} padding={2.5} borderRadius="xl" marginTop={2}>
+                                <Box>
+                                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
+                                        Token ID
+                                    </Box>
+                                    <Box as="h4" noOfLines={1} fontSize="sm">
+                                        {nft.tokenId}
+                                    </Box>
+                                </Box>
+                                <Box>
+                                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
+                                        Amount
+                                    </Box>
+                                    <Box as="h4" noOfLines={1} fontSize="sm">
+                                        {nft.amount}
+                                    </Box>
+                                </Box>
+                                <Box>
+                                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
+                                        Price
+                                    </Box>
+                                    <Box as="h4" noOfLines={1} fontSize="sm">
+                                        {buyerPrice.toString()} Wei
+                                    </Box>
+                                </Box>
+                                <Box>
+                                    <Box as="h4" noOfLines={1} fontWeight="medium" fontSize="sm">
+                                        ExchangeListingId
+                                    </Box>
+                                    <Box as="h4" noOfLines={1} fontSize="sm">
+                                        {id.toString()}
+                                    </Box>
+                                </Box>
+                            </SimpleGrid>
+                            <HStack marginTop={2}>
+                                <Button onClick={() => buyNFT()}>Buy</Button>
+                            </HStack>
+                        </>
+                    : <Typography>You bought this ticket!</Typography>
+            }
         </Box>
     );
 };
